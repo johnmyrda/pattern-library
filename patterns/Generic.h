34c4e5c6ffcb7232bee7b29293878214eaba4ce8
John <hello@johnmyrda.com>
2017-06-11 13:07:07 -0700

    Added 'repeat_length' parameter

--- patterns/Generic.h
+++ patterns/Generic.h
@@ -7,11 +7,16 @@ class Generic: public Pattern{
 
 public:
 
+  Generic(){}
+
   Generic(const CRGB * pattern, const int8_t pattern_length) :
     pattern(pattern), pattern_length(pattern_length){}
 
+  Generic(const CRGB * pattern, const int8_t pattern_length, const int8_t repeat_length) :
+      pattern(pattern), pattern_length(pattern_length), repeat_length(repeat_length){}
+
   void call(LedArray leds, uint16_t frame){
-    int pattern_index = frame % pattern_length;
+    int pattern_index = (frame/repeat_length) % (pattern_length);
     leds.push(pattern[pattern_index]);
   }
 
@@ -19,6 +24,7 @@ protected:
 
   const CRGB * pattern;
   uint8_t pattern_length;
+  uint8_t repeat_length = 1;
 
 };
 
