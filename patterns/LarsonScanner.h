b8e6a9a1f09b3429de3676e1ff9b6a4839405d79
John <hello@johnmyrda.com>
2017-06-11 10:52:45 -0700

    Colors are now picked in the constructor

--- patterns/LarsonScanner.h
+++ patterns/LarsonScanner.h
@@ -8,32 +8,26 @@ class LarsonScanner: public Pattern{
 
 public:
 
-  LarsonScanner(){};
+  //LarsonScanner(){};
+
+  LarsonScanner(const CRGB color1, const CRGB color2) : color1(color1), color2(color2){}
 
   void call(LedArray leds, uint16_t frame){
-    //frame_interval = 30;
-    //elapsed / frame_interval;
     // 0 == forwards, 1 = backwards
     int direction = (frame / leds.length) % 2;
     if(direction == 0){
-      CRGB next_color = CRGB::Black;
-      if(frame > leds.length){
-        //not the first time
-        next_color = complementary_color(*leds.tail);
-      } else if(frame == 0) {
-        //The first led the first time
-        next_color = random_color();
-      } else {
-        //later frames the first time
-        next_color = leds.get(1);
-      }
+      leds.push(color1);
     } else {
-      CRGB next_color = complementary_color(*leds.head);
-      leds.push_back(next_color);
+      leds.push_back(color2);
     }
   }
 
+protected:
+  const CRGB color1;
+  const CRGB color2;
+
 };
+
 
 
 #endif
