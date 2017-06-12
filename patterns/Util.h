4c9af6591ffe9f3d5aa14dfaafbcabe68ae0b34d
John <hello@johnmyrda.com>
2017-06-12 13:38:12 -0700

    Added function to get random contrasting colors

--- patterns/Util.h
+++ patterns/Util.h
@@ -3,8 +3,11 @@
 
 #include <FastLED.h>
 
+#define SATURATION 255
+#define VALUE 255
+
 CRGB random_color(){
-  return CHSV(random(255), 255, 255);
+  return CHSV(random(255), SATURATION, VALUE);
 }
 
 CRGB complementary_color(CRGB color){
@@ -13,6 +16,16 @@ CRGB complementary_color(CRGB color){
   CRGB rgb_complement;
   hsv2rgb_rainbow( hsv_color, rgb_complement);
   return rgb_complement;
+}
+
+void fillRandomContrastingColors(CRGB &c1, CRGB &c2) {
+  int h1 = random(255);
+  int offset = random(180) - 90;
+  int h2 = (h1 + 128 + offset) % 255;
+
+  //LOG("\tRandomContrastingColors generated\th1: %d\th2: %d", h1, h2);
+  c1 = CHSV(h1, SATURATION, VALUE);
+  c2 = CHSV(h2, SATURATION, VALUE);
 }
 
 #endif
