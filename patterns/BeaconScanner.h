3fb28c6b64ad0bb80dbb8e7c56eff131fc0102fd
John Myrda <jmyrda@cloudmark.com>
2017-06-12 13:48:33 -0700

    Baby's first beacon pattern

---
+++ patterns/BeaconScanner.h
@@ -0,0 +1,42 @@
+#ifndef BEACONSCANNER_H
+#define BEACONSCANNER_H
+
+#include "Pattern.h"
+#include "Util.h"
+#include "BeaconUtil.h"
+
+class BeaconScanner: public Pattern{
+
+public:
+
+  BeaconScanner(){
+    randomize();
+  };
+
+  BeaconScanner(const CRGB color) : color(color){}
+
+  void call(LedArray leds, uint16_t frame){
+    int16_t periodic_index = PERIOD/2 - ((frame + PERIOD/2) % (PERIOD));
+    uint8_t abs_periodic_index = abs(periodic_index);
+    for(uint16_t i = 0; i < leds.length; i++){
+      uint8_t stick_index = get_stick_index(i);
+      if(stick_index == abs_periodic_index){
+        leds.leds[i] = color;
+      } else {
+        leds.leds[i] = CRGB::Black;
+      }
+    }
+  }
+
+  void randomize(){
+    color = random_color();
+  }
+
+protected:
+  CRGB color;
+  static constexpr uint8_t PERIOD = (LEDS_PER_STICK - 1) * 2;
+};
+
+
+
+#endif
