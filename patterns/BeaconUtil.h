8d889d1d0cf7ea4f0f0bcabcf437a4f2cc9f5708
John Myrda <jmyrda@cloudmark.com>
2017-06-12 17:04:48 -0700

    Removed functions, added LEDS_PER_BEACON

--- patterns/BeaconUtil.h
+++ patterns/BeaconUtil.h
@@ -5,13 +5,10 @@
 
 static constexpr uint8_t LEDS_PER_STICK = 8;
 static constexpr uint8_t STICKS_PER_BEACON = 3;
-
-uint8_t get_beacon_index(uint16_t index){
-  return (index % (LEDS_PER_STICK * STICKS_PER_BEACON));
-}
+static constexpr uint8_t LEDS_PER_BEACON = (STICKS_PER_BEACON * LEDS_PER_STICK);
 
 uint8_t get_stick_index(uint16_t index){
-  uint8_t beacon_index = get_beacon_index(index);
+  uint8_t beacon_index = index % LEDS_PER_BEACON;
   uint8_t stick_index = beacon_index % LEDS_PER_STICK;
   if(beacon_index >= LEDS_PER_STICK && beacon_index < (LEDS_PER_STICK * 2)) {
     stick_index = LEDS_PER_STICK - stick_index - 1;
